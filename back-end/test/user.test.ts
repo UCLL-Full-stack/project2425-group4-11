1a71580019530123164e9f4763197fbab4a83677
Emma <r0997901@@ucll.be>
2024-10-29 11:43:20 +0100

    test added

--- back-end/test/user.test.ts
+++ back-end/test/user.test.ts
@@ -1,4 +1,12 @@
-const user = new User({
-    id: 
+import { User } from "../model/User";
 
-})
+const user = new User({
+    id: 1,
+    email: 'john.doe@example.com',
+    password: 'password123',
+    firstName: 'john',
+    lastName: 'doe',
+    phoneNumber: '0489342809',
+    accountStatus: true, 
+});
+
